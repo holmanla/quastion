1c499b689942681551352c6849dd30fd8f1ade45
holmanla <156009986+holmanla@users.noreply.github.com>
2024-03-26 16:50:46 -0400

    initializing with already-made files

---
+++ button_script.js
@@ -0,0 +1,6 @@
+document.getElementById('revealButton').addEventListener('click', function() {
+    document.getElementById('revealButton').classList.add('d-none');
+    document.getElementById('punchline').classList.remove('d-none');
+    document.getElementById('question').classList.add('d-none');
+  });
+  
